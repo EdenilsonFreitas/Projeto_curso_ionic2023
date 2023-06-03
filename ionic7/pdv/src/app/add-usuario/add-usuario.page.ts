8c9a5ba07e9c11e5b820624bda30777c61c6282e
Edenilson <freitaseddie126@gmail.com>
2023-06-03 00:44:19 -0300

    Post json, declarando Provider e passando Dados para API

--- ionic7/pdv/src/app/add-usuario/add-usuario.page.ts
+++ ionic7/pdv/src/app/add-usuario/add-usuario.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Api } from './../../services/api';
 
 @Component({
   selector: 'app-add-usuario',
@@ -14,16 +15,27 @@ export class AddUsuarioPage implements OnInit {
   senha: string = "";
   nivel: string = "";
   id: string = "";
-  constructor(private router:Router) { }
+  constructor(
+    private router:Router,
+    private provider: Api,
+    private actRouter: ActivatedRoute
+    
+    ) { }
 
   ngOnInit() {
+    // ACT ROUTER SERVE PARA RECEBER E PASSAR PARAMETROS ENTRE PÁGINAS
+    this.actRouter.params.subscribe((data:any)=>{
+
+    });
   }
   Usuarios(){
     this.router.navigate(['usuarios'])
   }
 
   cadastrar(){
-
+    return new Promise(resolve =>{
+      this.provider.dadosApi(dados, 'usuarios/inserir.php')
+    });
   }
 
   editar(){
--- ionic7/pdv/src/app/app.module.ts
+++ ionic7/pdv/src/app/app.module.ts
@@ -7,11 +7,19 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
+import { Api } from 'src/services/api';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [
+    Api,
+    { 
+    provide: RouteReuseStrategy,
+    useClass: IonicRouteStrategy
+   }
+  ],
+  
   bootstrap: [AppComponent],
 })
 export class AppModule {}
